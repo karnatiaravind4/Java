0e0159f4dfbb798a5c83735da2316936b9dd7835
karnatiaravind4 <karnatiaravind4@gmail.com>
2026-04-06 21:48:34 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class even{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+System.out.print("Enter the number:");
+int n=in.nextInt();
+if(n%2==0){
+System.out.println(n  +  "is a even number");
+}
+else{
+System.out.println(n  + "not a even number");
+}}}
